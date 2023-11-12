ba0d54e993c75d4db610a97dfac943b9340d63ca
due <im_trying@mail.ru>
2023-11-12 17:29:37 +0300

    added a timeout

--- src/state.gd
+++ src/state.gd
@@ -12,11 +12,29 @@ var should_leave: bool = false
 
 var current_points: int = 0
 
+func fadein(speed_mult: float = 1):
+	var colorrectplayer: AnimationPlayer = get_tree().current_scene.find_child("ColorRect").get_child(0)
+	colorrectplayer.speed_scale *= speed_mult
+	colorrectplayer.queue("fadein")
+
+func fadeout(speed_mult: float = 1):
+	var colorrectplayer: AnimationPlayer = get_tree().current_scene.find_child("ColorRect").get_child(0)
+	colorrectplayer.speed_scale *= speed_mult
+	colorrectplayer.queue("fadeout")
+
 func move_to_shop():
 	move_onto_other_level("res://shop/shop.tscn")
 
 func cops_arrive():
 	# get_tree().current_scene.find_children("*", "Officer")[0].show()
+	var timer := Timer.new()
+	get_tree().current_scene.add_child(timer)
+	timer.timeout.connect(__on_timer_timeout)
+	timer.wait_time = 0.5
+	timer.one_shot = true
+	timer.start()
+	
+func __on_timer_timeout():
 	cops_arrived.emit()
 	did_cops_arrive = true
 
