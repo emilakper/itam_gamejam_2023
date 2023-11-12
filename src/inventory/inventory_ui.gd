d866163a7766308c5f930bb33aa3e1ca9bfe26d8
m1n1tea <sir.makurov@ya.ru>
2023-11-12 15:35:10 +0300

    global inventory

--- src/inventory/inventory_ui.gd
+++ src/inventory/inventory_ui.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @export var inv_size : int = 5
-@export var inv : Inv
+@onready var inv : Inv = preload("res://inventory/global_inventory.tres")
 @onready var ui_slots = get_children()
 
 
@@ -9,15 +9,6 @@ extends Control
 func _ready():
 	inv.updated_slot.connect(update_slots)
 	update_slots()
-	inv.add(InvItem.get_item("blood"))
-	inv.add(InvItem.get_item("hair"))
-	
-	inv.use(InvItem.get_item("blood"))
-	
-	inv.add(InvItem.get_item("bag"))
-	inv.add(InvItem.get_item("blood"))
-	inv.add(InvItem.get_item("blood"))
-	inv.add(InvItem.get_item("empty_bag"))
 
 func update_slots():
 	for i in range(0,inv_size):
